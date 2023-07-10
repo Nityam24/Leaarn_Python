f03d9f347bd8fca8403a972c50bcf550deab4877
Nityam24 <nityampatel2402@gmail.com>
2023-07-10 07:35:12 +0530

    updated

--- rem_tuple.py
+++ rem_tuple.py
@@ -1,1 +1,14 @@
 # Write a program to remove an element from tuple.
+
+tuplex = "w", 3, "r", "s", "o", "u", "r", "c", "e"
+print(tuplex)
+
+tuplex = tuplex[:2] + tuplex[3:]
+print(tuplex)
+
+listx = list(tuplex) 
+
+listx.remove("c") 
+
+tuplex = tuple(listx)
+print(tuplex)
