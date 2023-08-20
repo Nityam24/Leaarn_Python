447cef524227234bdd7f1997824525ffd689bd08
Nityam24 <nityampatel2402@gmail.com>
2023-08-20 09:57:13 +0530

    new update

---
+++ internalexam.py
@@ -0,0 +1,23 @@
+# odd = [1, 3, 5]
+# odd.append(7)
+# print(odd)
+# odd.extend([9, 11, 13])
+# print(odd)
+# odd.append([7,6])
+# print(odd)
+
+
+# # Concatenating and repeating lists
+# odd = [1, 3, 5]
+# print(odd + [9, 7, 5])
+# print(["re"] * 3)
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = thislist.copy()
+# thislist[2]="bit"
+# print(mylist)
+# print(thislist)
+
+# a = "Hello, World!"
+# print(a.replace("H", "J"))
+
