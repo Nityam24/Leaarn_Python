ebde04b688dcdb8366de98e3f5ba9fe3d33c81b9
Nityam24 <nityampatel2402@gmail.com>
2023-08-20 10:16:09 +0530

    new update

--- internalexam.py
+++ internalexam.py
@@ -21,3 +21,22 @@
 # a = "Hello, World!"
 # print(a.replace("H", "J"))
 
+# a = " Hello, World! "
+# print(a.strip())
+
+# a = "Hello, World!"
+# print(a.split())
+# print(a.split(","))
+
+# txt = "apple#banana#cherry#orange"
+
+# # setting the maxsplit parameter to 1, will return a list with 2 elements!
+# x = txt.split("#", 4)
+
+# print(x)
+
+# quantity = 3
+# itemno = 567
+# price = 49
+# myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+# print(myorder.format(quantity, itemno, price))
