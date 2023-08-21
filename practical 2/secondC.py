8233a7fd24be550ac7d3e2e066b41caf851f7f38
Nityam24 <nityampatel2402@gmail.com>
2023-08-21 06:23:44 +0530

    new update

--- practical 2/secondC.py
+++ practical 2/secondC.py
@@ -4,3 +4,11 @@ temp=a
 a=b
 b=temp
 print("a, b and temp resp are: ",a,b,temp)
+
+
+#Write a program to swap the values of two variables without using temporary variable
+
+# a=float(input("enter the 1st number"))
+# b=float(input("enter the 2nd number"))
+# a,b=b,a
+# print("a and b are: ",a,b)
